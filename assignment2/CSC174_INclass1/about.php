9fd460e51c1fc21112e1f8117ee53cb02ab748ca
asingh34 <33998240+asingh34@users.noreply.github.com>
2019-02-11 17:59:44 -0500

    Add files via upload

---
+++ assignment2/CSC174_INclass1/about.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Anant Singh | About Me</title>
+   <link rel="stylesheet" href="css/styles.css">
+      <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300i" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Roboto:300i" rel="stylesheet">
+
+</head>
+<body class = container>
+   <header>
+      <a href = "index.php">
+         <h1> Anant singh</h1>
+         <img src ="images/anant.jpg" alt="ant">
+      </a>
+   </header>
+
+
+    <?php include "inc/aside.inc";?>
+
+
+   <main>
+      <h1 class = subHead>About Me</h1>
+      <p> The second law of thermodynamics tells us that Entropy, the degree of disorder, must always increase for a given system. Feynman measures disorder by the number of ways in which the insides of the system can be arranged so that from the outside it looks the same. In other words, systems will eventually trend toward the most probable outcome.
+
+      Thus a mixture of sand and pebbles will mix evenly throughout, and not have all pebbles sitting together; the universe will eventually reduce to dust; and most college students will study engineering, or the arts. These are the most probable outcomes.
+
+      Yet we have life, a highly improbable outcome given the molecules that comprise living organisms, perpetuating itself through reproduction! Schrodinger was moved to say that life feeds on negative entropy. And we have myself, a college student who is determined to study engineering, and the arts, with no compromise on either.
+
+      Conservative physicists dismiss "life" as merely a binding of the Sun's energy, doomed to perish along with it. Most engineering students dismiss the arts as soft; most art students regard the complexity of engineering courses with trepidation.
+
+      My conclusions are different. I believe that sentient life has no higher goal than to reduce the entropy of the universe. Thus our hunger for the improbable outcome, the novel and the unique; thus our quest for the origins of the universe; and thus indeed the rewards to the risk-taker. I hope to become an improbable type of engineer, one who builds useful as well as beautiful things.
+
+      My name is Anant Singh, and I am a rising sophomore pursuing a bachelor of arts in computer science at the University of Rochester. I was born and raised in Washington and graduated from University Prep in Seattle.</p>
+
+   </main>
+
+
+   <?php include "inc/footer.inc";?>
+
+   <?php include "inc/nav.inc";?>
+
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="js/menu-highlighter.js"></script>
+</body>
+</html>
---
+++ assignment2/CSC174_INclass1/works.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Anant Singh | Lab 1: Initial Personal Website</title>
+   <link rel="stylesheet" href="css/styles.css">
+      <link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300i" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Roboto:300i" rel="stylesheet">
+
+</head>
+<body class = container>
+   <header>
+      <a href = "index.php">
+         <h1> Anant singh</h1>
+         <img src ="images/anant.jpg" alt="ant">
+      </a>
+   </header>
+
+
+    <?php include "inc/aside.inc";?>
+
+
+
+   <main>
+      <h1 class = subHead>Works</h1>
+      see my other site <a href="asingh34.github.io"> asingh34.github.io</a>
+
+   </main>
+
+
+   <?php include "inc/footer.inc";?>
+
+   <?php include "inc/nav.inc";?>
+
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="js/menu-highlighter.js"></script>
+</body>
+</html>
