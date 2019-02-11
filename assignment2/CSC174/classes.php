413e13b4165ffe4d60339373df98725a4eb025f9
Jayde Jackson <47328486+bjacks16@users.noreply.github.com>
2019-02-11 18:09:41 -0500

    Add files via upload

---
+++ assignment2/CSC174/classes.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Jayde Jackson Lab 1</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link href="https://fonts.googleapis.com/css?family=Inconsolata|Staatliches" rel="stylesheet">
+</head>
+
+<body class="container">
+
+<?php include "inc/top.inc"; ?>
+
+<main>
+	<h1>Classes</h1>
+	<p>
+		The other computer science courses that I’ve taken are: 170 and 131. I plan to minor in computer science. I’ve also taken audio music engineering courses like 191 and 193. I’m really interested in sound design and creating music. I’ve worked in the music studio a lot with my uncle so that’s where I learned how to mix music. 
+
+	</p>
+</main>
+<?php include "inc/footer.inc"; ?>
+
+<script src="//code.jquery.com/jquery.js"></script>
+<script src="js/menu-highlighter.js"></script>
+</body>
+</html>
