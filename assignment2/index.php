14595802b280df4b0a1e218fe8ebb3cd7ec70937
Mike Henry <mhenry16@u.rochester.edu>
2019-02-11 18:32:36 -0500

    initial index contentt

--- assignment2/index.php
+++ assignment2/index.php
@@ -1,128 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">		
-<link rel="stylesheet"  href="./css/styles.css">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Prata" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Merienda+One" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Faster+One" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Staatliches" rel="stylesheet">		<title>Student | Jack D Dalton | Lab 1: Initial Personal Website</title>
-	</head>
-	<body class="container">
-		<header>
-	<a href="./index.php">
-		<h1>Jack Dalton</h1>
-		<img src="././images/jack_clubs.jpg" alt="Jack">
-	</a>	
-</header>		<aside>
-	<ul>
-		<li>Mechanical Engineering major</li> 
-		<li>Computer Science minor</li> 
-		<li>Graduating in 2020</li>
-	</ul>
-</aside>		<main>
-			<p>	I am a Mechanical Engineering major at the University of Rochester, and I am also minoring in Computer Science. I am very interested in how the world works, and I find both theoretical research and its application interesting. However, I decided to major in Mechanical Engineering because the idea of building something that could impact others interests me, which is why I also decided to minor in computer science. So far at UofR, I have taken the following courses: CSC 170, CSC 171, CSC 172, CSC 214, and CSC 240. Additionally, I have been a CHM 132 Lab Teacher Assistant, and I am currently a CSC 240/440 TA. 
-			</p>
-		</main>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Assignment 2 Flash Website</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<header>
+      <a href = "index.php">
+         <img src ="images/rochester.jpeg" alt="Rochester">
+      </a>
+   </header>
 
-		<footer>
-	CSC 174: Advanced Front-end Web 
-	Design and Development
-</footer>		<nav>
-	<ul>
-		<li>
-			<a href="./index.php">Home</a>
-		</li>
-		
-		<li>
-			<a href="./early_life.php">Early Life</a>
-		</li>
-		
-		<li>
-			<a href="./work_life.php">Work Life</a>
-		</li>
-		
-		<li>
-			<a href="./free_time.php">Free Time</a>
-		</li>
-	</ul>
-</nav>		<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
-<script src="http://code.jquery.com/jquery.js"></script>
-<script src="./js/menu-highlighter.js"></script>	
-<a href="http://jdalton.digitalscholar.rochester.edu/CSC174/Lab_01/">Original Site</a>
-</body>
-<header>
-	<a href="index.html">
-		<h1>Gen Galindo</h1>
-		<img src="images/genessis.jpg" alt="Genessis Galindo">
-	</a>
-</header>
+   <main>
+	  <h1>City</h1>
+	  
+	  <p>Rochester is a city in NY that only kinda sucks.</p>
 
-<aside>
-	<ul>
-		<li>Wannabe Master of Web Development</li>
-		<li>Ninja-level Event Planning Skills </li>
-		<li>Undergraduate Student at University of Rochester</li>
-	</ul>
-</aside>
+	  <h1>Team</h1>
 
-<main>
-	<p>I am a student majoring in Business, with a concentration in Analytics, and minoring in Computer Science and Spanish. Before coming to the University of Rochester, I attended Canyon High School in California where I was involved in various extracurricular such as: Student Government, National Honor Society, English National Honor Society, Spirit Club, Chemistry Club, and a few others. I even founded a chapter of the Science National Honor Society at my school and at the time I believed I wanted to major in Chemistry. </p>
-</main>
+	  <p>Michael is a senior Computer Science major from NY. Anant is a junior Computer Science major from Seattle. Jayde is ...</p>
 
-<footer>
-	CSC 174: Advanced Front-end Web
-	Design and Development
-</footer>
+   </main>
 
+   <?php include "inc/footer.inc";?>
 
-<nav>
-	<ul>
-		<li><a href="index.php">Home</a></li>
-		<li><a href="background.php">Background</a></li>
-		<li><a href="highschool.php">High School</a></li>
-		<li><a href="extra.php">Extracurriculars</a></li>
-	</ul>
-</nav>
+   <?php include "inc/nav.inc";?>
 
-<a href="http://ggalindo.digitalscholar.rochester.edu/csc174/lab01/">Original Site</a>
-
-</body>
-
-<script src="https://code.jquery.com/jquery.js"></script>
-<script src="js/menu-highlighter.js"></script>
-<header>
-    <a href="index.html">
-        <h1>Mike Henry</h1>
-        <img src="images/MichaelHenry.jpg" alt="Michael Henry">
-    </a>
-</header>
-<aside>
-    <ul>
-            <li>Wannabe Master of Web Development</li>
-            <li>Ninja-level Event Planning Skills </li>
-            <li>Undergraduate Student at University of Rochester</li>
-        </ul>
-    </aside>
-
-    <main>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sapien ante, luctus a bibendum vel, lobortis a erat. Cras non congue tellus. Ut vulputate, sapien vitae tincidunt pellentesque, ipsum lacus ultricies ante, in dignissim felis diam et erat. In nec mi id enim iaculis luctus in quis diam. </p>
-    </main>
-
-    <footer>
-        CSC 174: Advanced Front End
-        Web Development
-    </footer>
-
-    <nav>
-        <ul>
-            <li><a href="" class="active">Page 1</a></li>
-            <li><a href="">Page 2</a></li>
-            <li><a href="">Page 3</a></li>
-            <li><a href="">Page 4</a></li>
-        </ul>
-    </nav>
 </body>
 </html>
