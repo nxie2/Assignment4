f0c015aac76f777cea7d9cff66e23472ff582a01
Jayde Jackson <47328486+bjacks16@users.noreply.github.com>
2019-02-11 18:52:49 -0500

    Add files via upload

---
+++ CSC.174/jayde.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Jayde Jackson Lab 1</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link href="https://fonts.googleapis.com/css?family=Inconsolata|Staatliches" rel="stylesheet">
+</head>
+
+<body class="container">
+
+<?php include "inc/top.inc"; ?>
+
+<main>
+	<h1>Introduction</h1>
+	<p>
+		I’m from Brooklyn, NY. My family is from Trinidad. I grew up in Manhattan and went to school there. I’m a junior Digital Media Studies major. I went to an elementary school called ps41 and then the same place for middle and high school called NYC Lab High School. In 10th grade, I spent a year in Miami Florida. I only spent a year there, but I had a lot of fun. My hobby in high school was running track. Now I’m in cheer club twice a week. 
+
+	</p>
+		<h1>Classes</h1>
+	<p>
+		The other computer science courses that I’ve taken are: 170 and 131. I plan to minor in computer science. I’ve also taken audio music engineering courses like 191 and 193. I’m really interested in sound design and creating music. I’ve worked in the music studio a lot with my uncle so that’s where I learned how to mix music. 
+
+	</p>
+	<h1>Interests</h1>
+	<p>
+		I’m also really interested in photography. I enjoy taking pictures. Along with this class I’m also taking Digital Media Studies 103 where we’re learning more about photography and how to edit photos in Photoshop. We recently had to take photos and work on choosing a theme to move forward. 
+
+	</p>
+	<h1>Major</h1>
+	<p>
+		I ended up changing my major from International Relations last semester. I’m happy I made the change because I do feel like it fits me better. I’ve have worked on campus before. Before I worked at Starbucks and off campus at the Laser Lab. I took a lot of classes this semester, so my schedule is too full for work. 
+
+	</p>
+</main>
+<?php include "inc/footer.inc"; ?>
+
+<script src="//code.jquery.com/jquery.js"></script>
+<script src="js/menu-highlighter.js"></script>
+</body>
+</html>
