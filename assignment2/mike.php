e02dd3ed69afa73cfff04d0426acd02951e1d296
Mike Henry <mhenry16@u.rochester.edu>
2019-02-11 19:30:11 -0500

    content for mike.php

--- assignment2/mike.php
+++ assignment2/mike.php
@@ -19,45 +19,13 @@
 
    <main>
       <h1 class = subHead>About Me</h1>
-      <p> The second law of thermodynamics tells us that Entropy, the degree of disorder, must always increase for a given system. Feynman measures disorder by the number of ways in which the insides of the system can be arranged so that from the outside it looks the same. In other words, systems will eventually trend toward the most probable outcome.
+      <p>I grew up in Larchmont, NY, currently live in Rochester, NY, and next year will be moving to Richmond, VA. I am a senior at the University of Rochester</p>
 
-      Thus a mixture of sand and pebbles will mix evenly throughout, and not have all pebbles sitting together; the universe will eventually reduce to dust; and most college students will study engineering, or the arts. These are the most probable outcomes.
-
-      Yet we have life, a highly improbable outcome given the molecules that comprise living organisms, perpetuating itself through reproduction! Schrodinger was moved to say that life feeds on negative entropy. And we have myself, a college student who is determined to study engineering, and the arts, with no compromise on either.
-
-      Conservative physicists dismiss "life" as merely a binding of the Sun's energy, doomed to perish along with it. Most engineering students dismiss the arts as soft; most art students regard the complexity of engineering courses with trepidation.
-
-      My conclusions are different. I believe that sentient life has no higher goal than to reduce the entropy of the universe. Thus our hunger for the improbable outcome, the novel and the unique; thus our quest for the origins of the universe; and thus indeed the rewards to the risk-taker. I hope to become an improbable type of engineer, one who builds useful as well as beautiful things.
-
-      My name is Anant Singh, and I am a rising sophomore pursuing a bachelor of arts in computer science at the University of Rochester. I was born and raised in Washington and graduated from University Prep in Seattle.</p>
-
-      <h1 class = subHead>Relevant Coursework</h1>
-      <p>MTH 150 Discrete Mathematics:
-      •Logic, mathematical reasoning, introduction to proofs, mathematical induction, set operations, algorithms, introduction to number theory, recurrence relations, techniques of counting, graphs, as well as specific questions given by the “Towers of Hanoi,” and Euler’s “7 bridges of Konigsberg problem.”
-      •Grade received: B </p>
-
-      <p>CSC 172 Data Structures & Algorithms (Java):
-      •Basic data structures such as lists, stacks, queues, trees, hash tables, and graphs.
-      •Design and analysis.
-      •Various cost-benefit tradeoffs in designing, implementing, and using them.
-      •Implementation of the data structures in Java.
-      •Various algorithms including algorithms for searching and sorting,
-      recursion, and graph traversal.
-      •Grade received: B</p>
-
-      <p>CSC 171 Introduction to Computer Science (Java):
-      •Algorithmic thinking and computational problem solving.
-      •Fundamentals of Computer Programing.
-      •Software development tools and processes.
-      •Grade received: C+ </p>
-
-      <p>CSC 170 Web Design and Development:
-      •Created a web page using HTML, CSS, and JavaScript.
-      •Developed features allowing menu navigation, and information retrieval using JavaScript.
-      •Grade received: A</p>
+      <h1 class = subHead>Classes</h1>
+      <p>I will be graduating tthis spring witth a BS in computer science and a minor in math.</p>
 
       <h1 class = subHead>Works</h1>
-      see my other site <a href="asingh34.github.io"> asingh34.github.io</a>
+      see my other site <a href="mhenry16.github.io"> mhenry16.github.io</a>
    </main>
 
    <?php include "inc/footer.inc";?>
