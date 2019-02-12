20b91732a43174e54fb3ce2cbf0a4bdca48e3565
Anant Singh <asingh34@u.rochester.edu>
2019-02-11 23:19:21 -0500

    updated index.php

--- assignment3/index.php
+++ assignment3/index.php
@@ -8,17 +8,36 @@
 
 
 <body class = container>
+   <header>
+      <a href = "index.php">
+         <h1> Three Unrelated Related Pages</h1>
+      </a>
+   </header>
 
-<<<<<<< HEAD:assignment3/index.html
    <div class = headImage>
       <img src ="images/nike.jpg" alt="Nike Huraches">
+      <p>This page is about the history and different types of shoes. Ranging from sneakers, sandals, and boots. The page describes the differences between the shoes and their similarities in terms of construction and parts.  </p>
       <img src ="images/skittles.jpeg" alt="Skittles">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <img src ="images/question.png" alt="Picture Here">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
    </div>
 
-=======
+
+
+
    <?php include "inc/nav.inc";?>
->>>>>>> 1c85de2409a86d29546acf0a4c4ad16c3fd6e664:assignment3/index.php
+
 
    <script src="http://code.jquery.com/jquery.js"></script>
    <script src="js/menu-highlighter.js"></script>
